5e38fe28c58745e519f6de1bc8d7966db3bacad4
eli <helloworld.eli@gmail.com>
2020-06-09 10:03:43 +0800

    style: change varible name

--- scrapy_bloomfilter/bloomfilter.py
+++ scrapy_bloomfilter/bloomfilter.py
@@ -7,30 +7,31 @@ class HashMap(object):
         self.seed = seed
     
     def hash(self, value):
-        ret = 0
+        temp = 0
         for i in range(len(value)):
-            ret += self.seed * ret + ord(value[i])
-        return (self.m - 1) & ret
+            temp += self.seed * temp + ord(value[i])
+        return (self.m - 1) & temp
 
 
 class BloomFilter(object):
     def __init__(self, server, key, bit=BLOOMFILTER_BIT, hash_number=BLOOMFILTER_HASH_NUMBER):
-        self.m = 1 << bit
-        self.seeds = range(hash_number)
         self.server = server
         self.key = key
-        self.maps = [HashMap(self.m, seed) for seed in self.seeds]
+        self.m = 1 << bit
+        self.seeds = range(hash_number)
+        self.hash_maps = [HashMap(self.m, seed) for seed in self.seeds]
     
     def exists(self, value):
         if not value:
             return False
+
         exist = True
-        for map in self.maps:
-            offset = map.hash(value)
+        for hash_map in self.hash_maps:
+            offset = hash_map.hash(value)
             exist = exist & self.server.getbit(self.key, offset)
         return exist
     
     def insert(self, value):
-        for f in self.maps:
-            offset = f.hash(value)
+        for hash_map in self.hash_maps:
+            offset = hash_map.hash(value)
             self.server.setbit(self.key, offset, 1)
